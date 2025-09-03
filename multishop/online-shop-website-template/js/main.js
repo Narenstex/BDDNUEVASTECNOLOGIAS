337860667b5fdcb5ff749579071960051816434a
Naren <narensanchez18@gmail.com>
2025-09-03 01:40:15 -0500

    Naren Rojas: Actualizacion index, css y js

--- multishop/online-shop-website-template/js/main.js
+++ multishop/online-shop-website-template/js/main.js
@@ -31,5 +31,30 @@ function contarArticulos() {
   const articulos = document.getElementsByClassName('articulo').length;
   document.getElementById("contador").innerText = `Artículos visibles: ${articulos}`;
 }
+// Función de Naren: Ordenar artículos alfabéticamente
+function ordenarArticulos() {
+  let lista = document.querySelectorAll(".articulo");
+  let contenedor = document.getElementById("articulos");
+  let articulosArray = Array.from(lista);
+
+  articulosArray.sort((a, b) => a.innerText.localeCompare(b.innerText));
+  articulosArray.forEach(a => contenedor.appendChild(a));
+}
+
+// Función de Naren: Contar resúmenes visibles
+function contarResumidos() {
+  let visibles = document.querySelectorAll("p[id^='resumen']:not([style*='display:none'])").length;
+  alert("Hay " + visibles + " resúmenes abiertos.");
+}
+
+// Función de Naren: Resaltar palabra clave
+function resaltarPalabra() {
+  let palabra = document.getElementById("buscador").value;
+  let articulos = document.querySelectorAll(".articulo p, .articulo h3");
+
+  articulos.forEach(p => {
+    p.innerHTML = p.textContent.replace(new RegExp(palabra, "gi"), match => `<mark>${match}</mark>`);
+  });
+}
 
 window.onload = contarArticulos;
