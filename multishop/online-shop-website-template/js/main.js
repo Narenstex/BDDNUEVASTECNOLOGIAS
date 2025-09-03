500495cf9939779f031b6faa0cf2226f95e500e5
diegogarcia1305 <diegoalejandrogarcia1305@gmail.com>
2025-09-02 23:28:11 -0500

    Diego Garcia:actualización de index, css y js con aportes

--- multishop/online-shop-website-template/js/main.js
+++ multishop/online-shop-website-template/js/main.js
@@ -1,104 +1,35 @@
-(function ($) {
-    "use strict";
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+// DIEGO GARCIA: Función para mostrar/ocultar resumen
+function toggleResumen(id) {
+  const elem = document.getElementById(id);
+  if (elem.style.display === "none") {
+    elem.style.display = "block";
+  } else {
+    elem.style.display = "none";
+  }
+}
 
+// DIEGO GARCIA: Función para filtrar artículos por palabra clave
+function filtrarPorKeyword() {
+  const input = document.getElementById('buscador').value.toLowerCase();
+  const articulos = document.getElementsByClassName('articulo');
+  let count = 0;
 
-    // Vendor carousel
-    $('.vendor-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:3
-            },
-            768:{
-                items:4
-            },
-            992:{
-                items:5
-            },
-            1200:{
-                items:6
-            }
-        }
-    });
+  for (let i = 0; i < articulos.length; i++) {
+    const texto = articulos[i].innerText.toLowerCase();
+    if (texto.includes(input)) {
+      articulos[i].style.display = "block";
+      count++;
+    } else {
+      articulos[i].style.display = "none";
+    }
+  }
+  document.getElementById("contador").innerText = `Artículos visibles: ${count}`;
+}
 
+// DIEGO GARCIA: Función para contar artículos al cargar la página
+function contarArticulos() {
+  const articulos = document.getElementsByClassName('articulo').length;
+  document.getElementById("contador").innerText = `Artículos visibles: ${articulos}`;
+}
 
-    // Related carousel
-    $('.related-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            }
-        }
-    });
-
-
-    // Product Quantity
-    $('.quantity button').on('click', function () {
-        var button = $(this);
-        var oldValue = button.parent().parent().find('input').val();
-        if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        button.parent().parent().find('input').val(newVal);
-    });
-    
-})(jQuery);
-
+window.onload = contarArticulos;
